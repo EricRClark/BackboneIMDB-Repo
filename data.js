e246aca02d3e9bbc245688192170f54a60f08127
EricRClark <eric.erandy@gmail.com>
2016-03-14 17:50:34 -0400

    1st commit

---
+++ data.js
@@ -0,0 +1,26 @@
+var movies = [
+            {
+            title: "Exit Through the Gift Shop",
+            director: "Banksy",
+            posterImg: "p8043121_d_v8_aa.jpg",
+            desc: "Documentary"
+            },
+            {
+            title: "Bruno",
+            director: "Larry Charles",
+            posterImg: "bruno-brno.25877.jpg",
+            desc: "Love story"
+            },
+            {
+            title: "Blue Velvet",
+            director: "David Lynch",
+            posterImg: "p9492_p_v8_ah.jpg",
+            desc: "weird 80's flick"
+            },
+            {
+            title: "Life of Brian",
+            director: "Terry Jones",
+            posterImg: "images.jpeg",
+            desc: "Monty Python flick"
+            }
+];
